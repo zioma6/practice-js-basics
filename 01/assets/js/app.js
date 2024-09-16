70e3992be89e1828472a38059a60c0824dcb2f51
Agnieszka Graban <agnieszka.ziomkowska@gmail.com>
2024-09-16 13:34:01 +0200

    Solved 1 task

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,35 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof(a));
+console.log(typeof(b));
+
+const array = []
+
+const addResult = parseFloat(a) + b;
+console.log(addResult);
+array.push(addResult)
+
+const subtractResult = a - b;
+console.log(subtractResult);
+array.push(subtractResult)
+
+const multiResult = a * b;
+console.log(multiResult);
+array.push(multiResult)
+
+const divideResult = a / b;
+console.log(divideResult);
+array.push(divideResult)
+
+const moduloResult = parseFloat(a) % b;
+console.log(moduloResult);
+array.push(moduloResult)
+
+console.log(array)
+
+const newArray = array.filter(el => el > 20)
+
+console.log(newArray)
+
